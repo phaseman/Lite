1082627b58b21e0555b15946e362c18a98446bb1
Dawson Hessler <funkemunkybiz@gmail.com>
2021-03-21 13:35:59 -0400

    Adding PingSpoof, EXECUTOR_SERVICE, and fixing margin line conventions

--- src/main/java/me/rhys/client/Manager.java
+++ src/main/java/me/rhys/client/Manager.java
@@ -21,6 +21,7 @@ import me.rhys.client.module.movement.speed.Speed;
 import me.rhys.client.module.movement.step.Step;
 import me.rhys.client.module.player.InventoryMove;
 import me.rhys.client.module.player.NoRotate;
+import me.rhys.client.module.player.PingSpoof;
 import me.rhys.client.module.player.Timer;
 import me.rhys.client.module.player.disabler.Disabler;
 import me.rhys.client.module.player.nofall.NoFall;
@@ -43,9 +44,11 @@ public class Manager {
         event.register(
                 new HelpCommand("help", "shows you information about other commands"),
                 new ToggleCommand("toggle", "toggle <module>", "toggles a module", "t"),
-                new BindCommand("bind", "bind <add/remove> <module> <key>", "binds a key to a module"),
+                new BindCommand("bind", "bind <add/remove> <module> <key>",
+                        "binds a key to a module"),
                 new PluginsCommand("plugins", "shows you the servers plugins"),
-                new FriendCommand("friend", "friend add / remove <name>, list", "if you have friends..", "f"),
+                new FriendCommand("friend", "friend add / remove <name>, list",
+                        "if you have friends..", "f"),
                 new VClip("vclip", "teleport through blocks", "vc")
         );
     }
@@ -65,19 +68,26 @@ public class Manager {
                 new Sprint("Sprint", "Sprints for you", Category.MOVEMENT, Keyboard.KEY_NONE),
                 new Fly("Fly", "Fly like a faggot", Category.MOVEMENT, Keyboard.KEY_NONE),
                 new Speed("Speed", "move at un-legit speeds", Category.MOVEMENT, Keyboard.KEY_NONE),
-                new NoSlow("NoSlow", "Removes slowdown from items / blocks", Category.MOVEMENT, Keyboard.KEY_NONE),
+                new NoSlow("NoSlow", "Removes slowdown from items / blocks",
+                        Category.MOVEMENT, Keyboard.KEY_NONE),
                 new Step("Step", "Steps up blocks", Category.MOVEMENT, Keyboard.KEY_NONE),
                 new Sneak("Sneak", "Make your player sneak", Category.MOVEMENT, Keyboard.KEY_NONE),
 
                 new Timer("Timer", "Control game time", Category.PLAYER, Keyboard.KEY_NONE),
-                new InventoryMove("InventoryMove", "Allows you to move in your inventory", Category.PLAYER, Keyboard.KEY_NONE),
-                new NoRotate("NoRotate", "Prevents Server from changing head position", Category.PLAYER, Keyboard.KEY_NONE),
+                new PingSpoof("PingSpoof", "Spoof your connection latency",
+                        Category.PLAYER, Keyboard.KEY_NONE),
+                new InventoryMove("InventoryMove", "Allows you to move in your inventory",
+                        Category.PLAYER, Keyboard.KEY_NONE),
+                new NoRotate("NoRotate", "Prevents Server from changing head position",
+                        Category.PLAYER, Keyboard.KEY_NONE),
 
                 new HUD("HUD", "HUD for the client", Category.RENDER, Keyboard.KEY_NONE),
                 new ClickGui("ClickGui", "Opens the ClickUI", Category.RENDER, Keyboard.KEY_RSHIFT),
-                new Animations("Animations", "Change swing / block animations", Category.RENDER, Keyboard.KEY_NONE),
+                new Animations("Animations", "Change swing / block animations",
+                        Category.RENDER, Keyboard.KEY_NONE),
                 new Chams("Chams", "Chams?", Category.RENDER, Keyboard.KEY_NONE),
-                new Nametags("Nametags", "Display a player's nametag", Category.RENDER, Keyboard.KEY_NONE),
+                new Nametags("Nametags", "Display a player's nametag",
+                        Category.RENDER, Keyboard.KEY_NONE),
                 new NoHurtCam("NoHurtCam", "Hides hurtcam", Category.RENDER, Keyboard.KEY_NONE),
                 new NoFall("NoFall", "Prevents fall damage", Category.PLAYER, Keyboard.KEY_NONE),
                 new Disabler("Disabler", "Disables Anti-Cheats", Category.PLAYER, Keyboard.KEY_NONE),
